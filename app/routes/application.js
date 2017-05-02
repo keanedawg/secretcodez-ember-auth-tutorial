42f6110d18320c88f6b0e6bfa9c393672e39358a
keanedawg <cfife@chicagoventure.com>
2017-05-01 23:24:49 -0500

    fixed error

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,10 +1,14 @@
 import Ember from 'ember';
 
 // app/routes/application.js
+
 export default Ember.Route.extend({
-    actions: {
+
+  actions: {
     error: function(reason, transition) {
       this.transitionTo('/login');
       return false;
     }
+  }
+
 });
